90e5772b001d680f7c9e9ed0a11186f662446ec1
Sanjana Srivastava <32809192+sanjana11147@users.noreply.github.com>
2024-07-02 11:50:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='answer',
       version='0.1',
-      description='The answer to life, the universe',
+      description='The answer to life, the universe and everything.',
       url='https://github.com/sanjana11147/the-answer',
-      author='Sanjana',
+      author='Sanjana Srivastava',
       author_email='sanjana11147@gmail.com',
       license='MIT',
       packages=['answer'])
